442d841c34a687d86c108729f8abc5393f759128
Melanie Tai <mt337@cornell.edu>
2022-05-12 17:16:00 -0700

    Add crud functions to create and get inventory

--- myapp/crud/inventory_crud.py
+++ myapp/crud/inventory_crud.py
@@ -3,6 +3,29 @@
 from models import db, connect_to_db, Inventory
 
 
+def create_inventory(name, qty):
+    """Create and return an inventory."""
+
+    inventory = Inventory(name=name, qty=qty)
+
+    db.session.add(inventory)
+    db.session.commit()
+
+    return inventory
+
+
+def get_inventories():
+    """Return all inventories."""
+
+    return Inventory.query.all()
+
+
+def get_inventory_by_id(id):
+    """Return an inventory by primary key."""
+
+    return Inventory.query.get(id)
+
+
 if __name__ == "__main__":
     from myapp import app
 
