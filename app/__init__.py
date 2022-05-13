10a73486063d6d2ec87f29ab0706081a6070c115
Melanie Tai <mt337@cornell.edu>
2022-05-12 17:03:42 -0700

    Modify inital setup

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import jinja2
+
+app = Flask(__name__)
+
+# Normally, if you refer to an undefined variable in a Jinja template,
+# Jinja silently ignores this. This makes debugging difficult, so we'll
+# set an attribute of the Jinja environment that says to make this an
+# error.
+app.jinja_env.undefined = jinja2.StrictUndefined
+
+# This configuration option makes the Flask interactive debugger
+# more useful (you should remove this line in production though)
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+
+
--- run.py
+++ run.py
@@ -1,19 +1,5 @@
-"""Application Flask server"""
-from flask import Flask
 from app.models import connect_to_db
-import jinja2
-
-app = Flask(__name__)
-
-# Normally, if you refer to an undefined variable in a Jinja template,
-# Jinja silently ignores this. This makes debugging difficult, so we'll
-# set an attribute of the Jinja environment that says to make this an
-# error.
-app.jinja_env.undefined = jinja2.StrictUndefined
-
-# This configuration option makes the Flask interactive debugger
-# more useful (you should remove this line in production though)
-app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+from app import app
 
 
 if __name__=="__main__":
