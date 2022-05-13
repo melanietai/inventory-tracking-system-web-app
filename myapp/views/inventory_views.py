a9f10b8b857089dead539f86950b5a20557aa8e0
Melanie Tai <mt337@cornell.edu>
2022-05-12 17:17:26 -0700

    Add view function to return homepage and all inventories to overview

--- myapp/views/inventory_views.py
+++ myapp/views/inventory_views.py
@@ -2,3 +2,18 @@ from flask import (render_template, redirect, flash, request)
 from crud import inventory_crud
 from myapp import app
 
+@app.route("/")
+def index():
+    """Return homepage."""
+
+    return render_template("index.html")
+
+
+@app.route("/inventories")
+def get_inventories():
+    """Return all inventories."""
+
+    inventories = inventory_crud.get_inventories()
+
+    return render_template("overview.html", inventories=inventories)
+
