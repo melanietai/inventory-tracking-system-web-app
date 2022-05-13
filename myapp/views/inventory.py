246e896669602dc10ee77d4e87e100620dcebe01
Melanie Tai <mt337@cornell.edu>
2022-05-12 22:07:14 -0700

    Add view functions to return create page and edit page

--- myapp/views/inventory.py
+++ myapp/views/inventory.py
@@ -3,9 +3,20 @@ from myapp.crud import inventory_crud
 
 
 def index():
-    """Return all inventories."""
+    """Return all inventory items to overview page."""
 
-    inventories = inventory_crud.get_inventories()
+    all_inventory = inventory_crud.get_all_inventory()
 
-    return render_template("overview.html", inventories=inventories)
+    return render_template("overview.html", all_inventory=all_inventory)
 
+
+def create_inventory():
+    """Return create page to create a new inventory item."""
+
+    return render_template("create.html")
+
+
+def edit_inventory():
+    """Return edit page to edit an inventory item"""
+
+    return render_template("edit.html")
