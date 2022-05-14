c5c67a9609734c5e8da2955ab1f3508f5208f1f5
Melanie Tai <mt337@cornell.edu>
2022-05-14 00:12:26 -0700

    Complete unittests for Inventory view functions

---
+++ tests/test_inventory_views.py
@@ -0,0 +1,96 @@
+from myapp import app
+import unittest
+from myapp.models import connect_to_db, db, Inventory
+import os
+from myapp.crud.inventory_crud import get_inventory_by_id
+
+
+class InventoryViewsTestCase(unittest.TestCase):
+    """Tests for Inventory view functions."""
+
+    @classmethod
+    def setUpClass(cls):
+        # Get the Flask test client
+        cls.client = app.test_client()
+        app.config["TESTING"] = True
+
+        # Create test database tables
+        os.system("dropdb testdb")
+        os.system('createdb testdb')
+
+        # Connect to test database
+        connect_to_db(app, "postgresql:///testdb")
+
+    def setUp(self):
+        # Create tables
+        db.create_all()
+
+    def tearDown(self):
+        db.session.commit()
+        db.drop_all()
+
+    def test_index(self):
+        """Test return index.html"""
+
+        result = self.client.get("/")
+        
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"<h1>Welcome to your inventory tracking system</h1>", result.data)
+
+    def test_show_create_inventory_page(self):
+        """Test return create.html"""
+
+        result = self.client.get("/inventory/new")
+
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"<p>Create a new item in your inventory list.</p>", result.data)
+
+    def test_create_inventory(self):
+        """Test redirect to / after creating a new inventory item."""
+
+        result = self.client.post("/inventory/new", data={"name": "Shampoo", "qty": 500}, follow_redirects=True)
+        
+        self.assertEqual(result.status_code, 200)
+
+    def test_show_edit_inventory_page(self):
+        """Test return edit.html"""    
+
+        # Add sample data
+        shampoo = Inventory(name="shampoo", qty=500)
+        db.session.add(shampoo)
+        db.session.commit()
+
+        result = self.client.get(f"/inventory/{shampoo.id}")
+
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"<p>Update the product name or quantity.</p>", result.data)
+
+    def test_edit_inventory(self):
+        """Test redirect to / after editing an inventory item."""
+
+        # Add sample data
+        shampoo = Inventory(name="shampoo", qty=500)
+        db.session.add(shampoo)
+        db.session.commit()
+
+        result = self.client.post(f"/inventory/{shampoo.id}", data={"id": shampoo.id, "name": "Dove Shampoo", "qty": 400}, follow_redirects=True)
+        
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"<h1>Welcome to your inventory tracking system</h1>", result.data)
+
+    def test_delete_inventory(self):
+        """Test redircet to / after deleting an inventory item."""
+
+        # Add sample data
+        shampoo = Inventory(name="shampoo", qty=500)
+        db.session.add(shampoo)
+        db.session.commit()
+
+        result = self.client.post(f"/inventory/{shampoo.id}/delete", data={"id": shampoo.id}, follow_redirects=True)
+        
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"<h1>Welcome to your inventory tracking system</h1>", result.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
