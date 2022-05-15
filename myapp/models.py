31e731b3a29f86dcc49d7167269a2998f8c0bd59
Melanie Tai <mt337@cornell.edu>
2022-05-15 14:46:13 -0700

    Add Warehouse class

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,12 @@
-"""Models."""
+"""Models.
+
+A many-to-one relationship.
+An inventory item will be associated with one warehouse.
+A warehouse can have many inventory items.
+"""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
@@ -12,10 +18,26 @@ class Inventory(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String, nullable=False)
     qty = db.Column(db.Integer, nullable=False)
+    warehouse_id = db.Column(db.Integer, db.ForeignKey("warehouses.id"))
+
+    warehouse = db.relationship("Warehouse", backref=backref("inventories", cascade="all, delete"))
 
     def __repr__(self):
         return f"<Inventory id={self.id} name={self.name} qty={self.qty}>"
 
+
+class Warehouse(db.Model):
+    "A warehouse."
+
+    __tablename__="warehouses"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    # inventories = a list of Inventory objects
+
+    def __repr__(self):
+        return f"<Warehouse id={self.id} name={self.name}>"
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///inventory", echo=True):
