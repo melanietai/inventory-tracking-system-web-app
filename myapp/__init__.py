124d4328490b54bfce12ae0419ea66de99ef1f84
Melanie Tai <mt337@cornell.edu>
2022-05-15 15:46:13 -0700

    Decorate Warehouse view functions

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import jinja2
-from myapp.views import inventory as inventory_views
+from myapp.views import inventory as inventory_views, warehouse as warehouse_views, index as index_views
 
 app = Flask(__name__)
 
@@ -16,9 +16,18 @@ app.jinja_env.undefined = jinja2.StrictUndefined
 # more useful (you should remove this line in production though)
 app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
 
-app.add_url_rule('/', view_func=inventory_views.index)
-app.add_url_rule('/inventory/new', view_func=inventory_views.show_create_inventory_page)
-app.add_url_rule('/inventory/new', view_func=inventory_views.create_inventory, methods=['POST'])
+app.add_url_rule('/', view_func=index_views.index)
+
+app.add_url_rule('/inventory/new', view_func=inventory_views.show_new_inventory_page)
+app.add_url_rule('/inventory/new', view_func=inventory_views.new_inventory, methods=['POST'])
 app.add_url_rule('/inventory/<id>', view_func=inventory_views.show_edit_inventory_page)
 app.add_url_rule('/inventory/<id>', view_func=inventory_views.edit_inventory, methods=['POST'])
 app.add_url_rule('/inventory/<id>/delete', view_func=inventory_views.delete_inventory, methods=['POST'])
+
+app.add_url_rule('/warehouse/new', view_func=warehouse_views.show_new_warehouse_page)
+app.add_url_rule('/warehouse/new', view_func=warehouse_views.new_warehouse, methods=['POST'])
+app.add_url_rule('/warehouse/<id>', view_func=warehouse_views.show_edit_warehouse_page)
+app.add_url_rule('/warehouse/<id>', view_func=warehouse_views.edit_warehouse, methods=['POST'])
+app.add_url_rule('/warehouse/<id>/delete', view_func=warehouse_views.delete_warehouse, methods=['POST'])
+
+
